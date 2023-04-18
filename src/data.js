430b91117eb685631e154c1877f9974321e1d024
FReptar0 <20213tn137@utez.edu.mx>
2023-04-18 09:11:59 -0600

    ip

--- src/data.js
+++ src/data.js
@@ -1,3 +1,3 @@
 // exporta una variable con un saludo
-export const path = "http://54.85.25.230:8080/";
-export const pathSocket = "54.85.25.230:3000";
+export const path = "http://54.226.152.202:8080/";
+export const pathSocket = "54.226.152.202:3000";
